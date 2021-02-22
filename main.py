c933049db816022a704357157d1af6271ddb23e7
Joelle Lindsey <joelle.gartner@shaw.ca>
2021-02-22 23:25:00 +0000

    data types

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 name = input('What is your name? ')
 print('hellooooo ' + name)
+#Fundamental Data typesT
+#int - integer
+#float - float point
+#bool - true/false
+#str - string
+#list - 
+#tuple
+#set
+#dict
+
+#Classes - customized types
+
+#Specialized Data Types - packages and modules from libraries
