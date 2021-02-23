ed5b92dca787ab9a35d56955a366cd9d914aea1a
Joelle Lindsey <joelle.gartner@shaw.ca>
2021-02-23 03:26:07 +0000

    learning python numbers

--- main.py
+++ main.py
@@ -2,7 +2,18 @@ name = input('What is your name? ')
 print('hellooooo ' + name)
 #Fundamental Data typesT
 #int - integer
+print(2+4)
+print(type(2+4))
 #float - float point
+print(type(9.9+1.1))
+print(2**3) #2 to power of 3
+print(3//4) #rounds value to integer
+print(5%4) #remainder
+
+
+#math functions
+print(round(3.1)) #rounds number
+print(abs(-20)) #returns absolute value
 #bool - true/false
 #str - string
 #list - 
