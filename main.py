c5061b09d312522a52863a8e287f89661e75c256
Joelle Lindsey <joelle.gartner@shaw.ca>
2021-03-03 23:42:39 +0000

    list practice

--- main.py
+++ main.py
@@ -140,7 +140,15 @@ dictionary={
 
 print(dictionary['b'])
 
+#fix this code so that it prints a sorted list of all of our friends (alphabetical). Scroll to see answer
+friends = ['Simon', 'Patty', 'Joy', 'Carrie', 'Amira', 'Chu']
 
+new_friend = ['Stanley']
+
+friends.insert(len(friends), new_friend[0])
+friends.sort()
+
+print(friends.sort())
 
 #Classes - customized types
 
