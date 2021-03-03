f90a665da77be4a496a46013f89f54046fa14f17
Joelle Lindsey <joelle.gartner@shaw.ca>
2021-03-03 16:38:02 +0000

    list unpacking

--- main.py
+++ main.py
@@ -122,6 +122,14 @@ new_sentence = sentence.join(['hi', 'my', 'name', 'is', 'jojo'])
 
 print(new_sentence)
 
+#list unpacking
+a,b,c, *other, d = [1,2,3,4,5,6,7,8,9]
+
+print(a)
+print(b)
+print(c)
+print(other)
+
 #tuple
 #set
 #dict
