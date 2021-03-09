b60dc0cb4032e53e0f3cc146e705b10335df7d30
Joelle Lindsey <joelle.gartner@shaw.ca>
2021-03-09 23:14:03 +0000

    ternary

--- learn2.py
+++ learn2.py
@@ -8,3 +8,14 @@ elif is_licenced:  #else if statement
     print('you can drive now')
 else:
     print('checkcheck')
+
+
+#ternary operator (shortcut if else)
+
+#condition_if_true if condition else condition_if_else
+
+is_friend = True
+
+can_message = "message allowed" if is_friend else "not allowed to message"
+
+print(can_message)
