2deb10d1e00c5829ccb61446fd009a4c63f24f84
Joelle Lindsey <joelle.gartner@shaw.ca>
2021-03-11 23:18:55 +0000

    logical operators

--- learn2.py
+++ learn2.py
@@ -38,5 +38,5 @@ if is_magician and is_expert:
     print('you are a master magician')
 elif is_magician and not is_expert:
     print('''at least you're getting there''')
-else:
+elif not is_magician:
     print('you need magic powers')
