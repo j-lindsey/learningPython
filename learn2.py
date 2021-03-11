2d1550b9497dc873bc130e00d56e29a257009f27
Joelle Lindsey <joelle.gartner@shaw.ca>
2021-03-11 23:18:00 +0000

    logical operators

--- learn2.py
+++ learn2.py
@@ -28,3 +28,15 @@ print(is_Friend and is_User)
 
 if is_Friend or is_User:
     print('best friends forever')
+
+print(not (True))
+
+is_magician = False
+is_expert = True
+
+if is_magician and is_expert:
+    print('you are a master magician')
+elif is_magician and not is_expert:
+    print('''at least you're getting there''')
+else:
+    print('you need magic powers')
