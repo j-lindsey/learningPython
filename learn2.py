b2ae195842e849f79c167540c6bfa6ff8c3ccf81
Joelle Lindsey <joelle.gartner@shaw.ca>
2021-03-22 15:41:33 +0000

    list practice

--- learn2.py
+++ learn2.py
@@ -66,3 +66,14 @@ for item in user.values():
 
 for item in user.keys():
     print(item)
+
+for key, value in user.items():
+    print(key, value)
+
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+sum = 0
+for item in my_list:
+    sum += item
+
+print(sum)
